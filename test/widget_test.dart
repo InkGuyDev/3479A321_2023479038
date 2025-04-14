6355aebd252dbbe592b12923e85e058677660e6a
Rodrigo Ignacio Díaz Muñoz <rdiaz23@alumnos.utalca.cl>
2025-04-14 15:39:39 -0400

    Lab4 listo ahora si

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:application_laboratorio/main.dart';
+import 'package:application_laboratorio/pages/myhomepage.dart';
+//import 'package:application_laboratorio/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
